b646ff8450baa64d58d8b33cfd1a94f82a006362
mmm00007 <maxime.mansour@proton.me>
2026-02-20 21:47:52 +0400

    Add React Query client wiring to app root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App'
+import { getQueryClient } from './app/queryClient'
 import './index.css'
+
+const queryClient = getQueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {import.meta.env.DEV ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+    </QueryClientProvider>
   </React.StrictMode>
 )
